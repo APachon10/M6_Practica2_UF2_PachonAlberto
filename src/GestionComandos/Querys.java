5b7aa6b750c60511363950b8f1f48e812f4a990a
Alberto <Pachondestroy95@gmail.com>
2019-11-06 20:22:38 +0100

    Procedimiento Insertar Socio FInalizado

--- src/GestionComandos/Querys.java
+++ src/GestionComandos/Querys.java
@@ -1,5 +1,58 @@
 package GestionComandos;
 
-public class Querys {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import Ejercicios.Conexion;
+import Interfaces.ParametrosConexion;
+
+public class Querys implements ParametrosConexion{
+	public void insertar() {
+		Conexion c  = new Conexion();
+		c.conectar(ParametrosConexion.url, ParametrosConexion.user, ParametrosConexion.password);
+		try {
+			String query_insert = "insert into socio values(?,?,?,?,?,?,?,?,?,?,?,?)";
+			PreparedStatement pst = ((Connection) c).prepareStatement(query_insert);
+			
+			//Ponemos los Parametros
+			/*pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst
+			pst*/
+		} catch (Exception e) {
+			System.out.println("Error!");
+			System.out.println("===================");
+			e.printStackTrace();
+		}
+	}
+	public void update() {
+		Conexion c  = new Conexion();
+		c.conectar(ParametrosConexion.url, ParametrosConexion.user, ParametrosConexion.password);
+		try {
+			String query_update = "";
+		} catch (Exception e) {
+			System.out.println("Error!");
+			System.out.println("===================");
+			e.printStackTrace();
+		}
+	}
+	public void delete() {
+		Conexion c  = new Conexion();
+		c.conectar(ParametrosConexion.url, ParametrosConexion.user, ParametrosConexion.password);
+		try {
+			String query_delete = "";
+		} catch (Exception e) {
+			System.out.println("Error!");
+			System.out.println("===================");
+			e.printStackTrace();
+		}
+	}
 }
