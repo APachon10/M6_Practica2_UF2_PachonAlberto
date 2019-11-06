56228c2385720055eb0986d3baa80f9580404805
Alberto <Pachondestroy95@gmail.com>
2019-11-06 19:59:31 +0100

    Conexion sin pasar Parameetros

--- src/Ejercicios/Conexion.java
+++ src/Ejercicios/Conexion.java
@@ -1,5 +1,22 @@
 package Ejercicios;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Conexion {
+	public Connection conectar() {
+		// MySQL
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_videoclub",
+					"root",
+					"");
+			System.out.println("Acceso Ejecutado correctamente ");
+		} catch (Exception e) {
+			System.out.println("Error!");
+			System.out.println("===================");
+			e.printStackTrace();
+		}
+		return conexion;
+	}
 }
--- src/Ejercicios/Principal.java
+++ src/Ejercicios/Principal.java
@@ -1,5 +1,8 @@
 package Ejercicios;
 
 public class Principal {
-
-}
+	public static void main(String[] args) {
+		Conexion c = new Conexion();
+		c.conectar();
+	}
+}	
